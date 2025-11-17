c2d79e51632c61659fc9d6335880a143c20809ce
Lauren Hruza <113564959+PenDr0p@users.noreply.github.com>
2025-11-16 21:43:29 -0500

    added slider with experimental code

--- Assets/vuforia_manager.cs
+++ Assets/vuforia_manager.cs
@@ -5,15 +5,28 @@ using UnityEngine.UI;
 public class VuforiaManager : MonoBehaviour
 {
     public Button toggleButton;
+    public Renderer targetRenderer; // Drag your object here in the Inspector
+    public Slider transparencySlider; // Drag your UI Slider here
 
     void Start()
     {
+        TargetController.currentlyTracked.ToggleCube();
         toggleButton.onClick.AddListener(ToggleAR);
     }
     
     public void GoToHome(){
         SceneManager.LoadScene("MenuScene");
     }
+
+    public void UpdateTransparency()
+    {
+        targetRenderer = TargetController.currentlyTracked;
+        Color objectColor = targetRenderer.material.color;
+        // Set the alpha (A) component of the color to the slider's value
+        objectColor.a = transparencySlider.value;
+        targetRenderer.material.color = objectColor;
+    }
+
 
     public void ToggleAR()
     {
